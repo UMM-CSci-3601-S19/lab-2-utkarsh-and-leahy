58b015cf53fd65a044cba34d35045d0b3fae71cb
leahy089 <leahy089@morris.umn.edu>
2019-02-17 20:52:43 -0600

    Finish setup of limit command. Start to filter by status.
    Added html button and connected to limit function made by Karsh.
    Started on filtering by status by adding radio buttons to html with partially finished javascript functions associated.

--- src/main/resources/public/javascript/todo.js
+++ src/main/resources/public/javascript/todo.js
@@ -8,6 +8,32 @@ function getAllTodos() {
   });
 }
 
+function getTodosByLimit() {
+  console.log("Getting the specified amount of todos.");
+
+  var httpClient = new HttpClient();
+  httpClient.get("/api/todo?limit=" + document.getElementById("limit").value, function (returned_json) {
+    document.getElementById('jsonDumpTodos').innerHTML = returned_json;
+  });
+}
+
+function getAllCompleteTodos() {
+  console.log("Filtering out the incomplete todos")
+
+  /*var httpClient = new HttpClient();
+  httpClient.get("/api/todo?limit=" + document.getElementById("limit").value, function (returned_json) {
+    document.getElementById('jsonDumpTodos').innerHTML = returned_json;
+  });*/
+}
+
+function getAllIncompleteTodos() {
+  console.log("Filtering out the complete todos")
+
+  /*var httpClient = new HttpClient();
+  httpClient.get("/api/todo?limit=" + document.getElementById("limit").value, function (returned_json) {
+    document.getElementById('jsonDumpTodos').innerHTML = returned_json;
+  });*/
+}
 
 function HttpClient() {
   this.get = function (aUrl, aCallback) {
