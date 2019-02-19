cc88dc1414e5849fb40ecd619dae6d1890463dcb
UtkarshKr007 <kumar375@morris.umn.edu>
2019-02-18 23:08:51 -0600

    Create Test for Filtering Todo By Limit
    Create user3601.todo file in test for todo tests

---
+++ src/test/java/umm3601/todo/FilterTodoByLimit.java
@@ -0,0 +1,25 @@
+package umm3601.todo;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class FilterTodoByLimit {
+
+  @Test
+  public void filterTodoByLimit() throws IOException {
+    TodoDatabase db = new TodoDatabase("src/main/data/todos.json");
+    Todo[] allTodos = db.listTodos(new HashMap<>());
+
+    Todo[] limit5Todos = db.limitTodo(allTodos, 5);
+    assertEquals("Incorrect number of todos with limit 5", 3, limit5Todos.length);
+
+    Todo[] limit10Todos = db.limitTodo(allTodos, 10);
+    assertEquals("Incorrect number of todos with limit 10", 1, limit10Todos.length);
+  }
+  
+}
